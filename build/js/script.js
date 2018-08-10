37feb14784385aaa5508643a8118311b19f60a04
asifalmani <asif.shahzad@coeus-solutions.de>
2018-08-10 17:24:33 +0500

    build folder deleted

--- build/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(e){
-    $('.selectpicker').selectpicker();
-
-    $('.input-group__search .input-group-text').on('click', function(e) {
-       $current =  $(this);
-       $current.closest('.input-group__search').toggleClass('active__search');
-    });
-});
